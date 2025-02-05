cdfee92920d4a8fbdffa4b8aafcde54d258aacb4
bhushantile20 <bhushantile2003@gmail.com>
2025-02-05 14:04:32 +0530

    commiting changes

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,7 @@
+INSERT INTO PRODUCT (name, desc, brand, price, category, release_date, available, quantity)
+VALUES
+('Smartphone', 'Latest Android Smartphone', 'BrandA', 699.99, 'Electronics', '2024-01-01', true, 100),
+('Washing Machine', 'Energy-efficient washer', 'BrandB', 499.50, 'Home Appliances', '2023-11-15', false, 0),
+('Office Chair', 'Ergonomic and durable', 'BrandC', 199.99, 'Furniture', '2023-07-10', true, 25),
+('Jeans', 'Comfortable denim jeans', 'BrandD', 39.99, 'Clothing', '2023-05-20', true, 150),
+('Toy Car', 'Electric remote-controlled car', 'BrandE', 29.99, 'Toys', '2023-12-05', false, 0);
